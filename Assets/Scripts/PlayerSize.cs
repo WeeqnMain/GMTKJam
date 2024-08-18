08176c82a248db275c886b236a78ef1766f31cf1
Witold Hołdys <152323163+WitHol@users.noreply.github.com>
2024-08-18 17:35:49 +0200

    refactor: renamed the ground, ceiling and walls

--- Assets/Scripts/PlayerSize.cs
+++ Assets/Scripts/PlayerSize.cs
@@ -5,32 +5,29 @@ using UnityEngine;
 
 public class PlayerSize : MonoBehaviour
 {
-    [SerializeField] private TarodevController.PlayerStats stats;
+  [SerializeField] private TarodevController.PlayerStats stats;
 
-    [SerializeField] private float scaleIncrease;
-    [SerializeField] private float massIncrease;
-    [SerializeField] private float speedDecrease;
+  [SerializeField] private float scaleIncrease;
+  [SerializeField] private float massIncrease;
+  [SerializeField] private float speedDecrease;
     
-    private Vector3 baseScale;
+  private Vector3 baseScale;
 
-    private Rigidbody2D rigidbody;
-
-    private void Awake()
+  private Rigidbody2D rigidbody;
+  private void Awake()
+  {
+      baseScale = gameObject.transform.localScale;
+      rigidbody = GetComponent<Rigidbody2D>();
+  }
+  
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.gameObject.CompareTag("Food")) 
     {
-        baseScale = gameObject.transform.localScale;
-
-        rigidbody = GetComponent<Rigidbody2D>();
+      gameObject.transform.localScale += Vector3.one * scaleIncrease;
+      rigidbody.mass += massIncrease;
+      stats.BaseSpeed -= speedDecrease;
+      Destroy(other.gameObject);
     }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-      if (other.gameObject.CompareTag("Food")) 
-      {
-        gameObject.transform.localScale += Vector3.one * scaleIncrease;
-        rigidbody.mass += massIncrease;
-        stats.BaseSpeed -= speedDecrease;
-
-        Destroy(other.gameObject);
-      }
-    }
+  }
 }
