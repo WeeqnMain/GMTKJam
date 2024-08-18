6b49442ad8823404c11c3ce19ce0239de602f60b
Witold Hołdys <152323163+WitHol@users.noreply.github.com>
2024-08-18 19:56:15 +0200

    Made seeds hang on tree rather that fall

--- Assets/Scripts/PlayerSize.cs
+++ Assets/Scripts/PlayerSize.cs
@@ -1,32 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using TarodevController;
+using TMPro;
 using UnityEngine;
 
 public class PlayerSize : MonoBehaviour
 {
   [SerializeField] private TarodevController.PlayerStats stats;
-
+  [SerializeField] private TextMeshProUGUI outputText;
+  [SerializeField] private float initialSpeed;
   [SerializeField] private float scaleIncrease;
   [SerializeField] private float massIncrease;
   [SerializeField] private float speedDecrease;
-    
+  
   private Vector3 baseScale;
+  private int seedsEaten;
 
   private Rigidbody2D rigidbody;
   private void Awake()
   {
-      baseScale = gameObject.transform.localScale;
       rigidbody = GetComponent<Rigidbody2D>();
+      baseScale = gameObject.transform.localScale;
+      seedsEaten = 0;
+      stats.BaseSpeed = initialSpeed;
   }
   
   void OnTriggerEnter2D(Collider2D other)
   {
     if (other.gameObject.CompareTag("Food")) 
     {
+      ++seedsEaten;
       gameObject.transform.localScale += Vector3.one * scaleIncrease;
       rigidbody.mass += massIncrease;
       stats.BaseSpeed -= speedDecrease;
+      outputText.text = seedsEaten.ToString();
+
       Destroy(other.gameObject);
     }
   }
