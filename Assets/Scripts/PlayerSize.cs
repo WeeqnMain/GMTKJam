5d2cb0f60fb145cc7f31ea62fbc718beea002a43
weeqn <gugobute@gmail.com>
2024-08-19 17:13:36 +0300

    feat: remade seed managing logic

--- Assets/Scripts/PlayerSize.cs
+++ Assets/Scripts/PlayerSize.cs
@@ -1,50 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using TarodevController;
 using TMPro;
 using UnityEngine;
 
 public class PlayerSize : MonoBehaviour
 {
-  [SerializeField] private TarodevController.PlayerStats stats;
-  [SerializeField] private TextMeshProUGUI outputText;
-  [SerializeField] private float initialSpeed;
-  [SerializeField] private float scaleIncrease;
-  [SerializeField] private float massIncrease;
-  [SerializeField] private float speedDecrease;
+    [SerializeField] private PlayerStats stats;
+    [SerializeField] private float initialSpeed;
+    [SerializeField] private float scaleIncrease;
+    [SerializeField] private float massIncrease;
+    [SerializeField] private float speedDecrease;
   
-  private Vector3 baseScale;
-  private int seedsEaten;
-  private float cooldown;
-  private Rigidbody2D rigidbody;
+    private float cooldown;
+    private Rigidbody2D rigidbody;
 
-  private void Awake()
-  {
-      rigidbody = GetComponent<Rigidbody2D>();
-      baseScale = gameObject.transform.localScale;
-      seedsEaten = 0;
-      stats.BaseSpeed = initialSpeed;
-  }
-  
-  private void Update()
-  {
-    if(cooldown > 0) cooldown -= Time.deltaTime;
-  }
-
-  void OnTriggerEnter2D(Collider2D other)
-  {
-    if (other.gameObject.CompareTag("Food") && cooldown <= 0) 
+    private void Awake()
     {
-      cooldown = 0.5f;
-
-      ++seedsEaten;
-      gameObject.transform.localScale += Vector3.one * scaleIncrease;
-      rigidbody.mass += massIncrease;
-      stats.BaseSpeed -= speedDecrease;
-      outputText.text = seedsEaten.ToString();
-
-      Destroy(other.gameObject);
+        rigidbody = GetComponent<Rigidbody2D>();
+        stats.BaseSpeed = initialSpeed;
     }
-  }
+  
+    private void Update()
+    {
+        if(cooldown > 0) cooldown -= Time.deltaTime;
+    }
+
+    public void IncreaseSize()
+    {
+        cooldown = 0.5f;
+        gameObject.transform.localScale += Vector3.one * scaleIncrease;
+        rigidbody.mass += massIncrease;
+        stats.BaseSpeed -= speedDecrease;
+    }
 }
